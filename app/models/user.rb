efb609e4f6bcb8bdff831fc377239f309a803082
lilian annette <lilianannettepro@gmail.com>
2023-11-28 16:00:24 +0100

    creating model review correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :reviews, foreign_key: 'author_id'
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
