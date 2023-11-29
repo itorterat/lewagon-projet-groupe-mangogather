f00aa26e5ac7e0cc184e9a3d58136afd8cae77d4
yaya <y.niang0@laposte.net>
2023-11-29 14:19:11 +0100

    search-barre, correction view Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @users = User.all
+    @users = @users.where(city: params[:city]) if params[:city].present?
+
+    if params[:category].present?
+      category = Category.find_by(name: params[:category])
+      @users = @users.joins(:services).where(services: { category_id: category.id }) if category
+    end
+
+    @users = @users.distinct
   end
 
   def show
-    @user = User.find_by_username(params[:id])
+    @user = User.find(params[:id])
   end
 end
