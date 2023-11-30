cc4f4d41a3a402ad49a84632e19fec47a52fc70f
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-30 21:36:50 +0100

    Add an edit profile to customize our own profile
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_user, only: %i[edit_profile update_profile]
 
   def index
     @users = User.all
@@ -16,5 +17,28 @@ class UsersController < ApplicationController
   def show
     @booking = Booking.new
     @user = User.find(params[:id])
+  end
+
+  def edit_profile
+    @categories = Category.all.order(:name)
+  end
+
+  def update_profile
+    if @user.update(user_params)
+      redirect_to edit_profile_path, notice: 'Profil mis à jour avec succès.'
+    else
+      @categories = Category.all.order(:name)
+      render :edit_profile
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :city, :description, category_ids: [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   resources :users, only: %i[index show]
+  get 'profile', to: 'users#edit_profile', as: :edit_profile
+  patch 'profile', to: 'users#update_profile', as: :update_profile
+
   resources :bookings, only: %i[index new create show]
+
   get 'search', to: 'pages#search'
 end
