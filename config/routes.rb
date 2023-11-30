cfc600b94618ab0d6d3efe57bbfc1320b0bbfb75
yaya <y.niang0@laposte.net>
2023-11-30 12:36:51 +0100

    bookings-controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :users, only: %i[index show]
+  resources :bookings, only: %i[index new create show]
   get 'search', to: 'pages#search'
 end
